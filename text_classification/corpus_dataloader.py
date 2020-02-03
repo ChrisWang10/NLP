581ff3be84c26add8486bb65164fd0280401f67a
ChrisWang10 <wchao118@gmail.com>
2020-02-03 17:53:27 +0800

    changed

--- text_classification/corpus_dataloader.py
+++ text_classification/corpus_dataloader.py
@@ -1,6 +1,8 @@
 from torch.utils.data import Dataset
 import torch.nn.utils.rnn as rnn_utils
 import pickle
+import os
+from config import Cfg
 
 
 class MyTrainValData(Dataset):
@@ -8,7 +10,7 @@ class MyTrainValData(Dataset):
         super(MyTrainValData, self).__init__()
         self.data = data
         self.embeddings = pickle.load(
-            open(r'C:\Users\king\Documents\code\NLP\text_classification\data\emb_weights.pkl', 'rb'))
+            open(os.path.join(Cfg().word2vec_from_scratch, 'emb_weights.pkl'), 'rb'))
 
     def __len__(self):
         assert len(self.data[0]) == len(self.data[1])
